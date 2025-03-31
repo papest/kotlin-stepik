8fc8811e99954ee8213233db4b538a48a7b395f0
Epap <peia@inbox.ru>
2025-03-31 23:45:36 +0300

    Марио 5 actions, 3 states

--- src/main/kotlin/mario/mario.kt
+++ src/main/kotlin/mario/mario.kt
@@ -1,41 +1,67 @@
 package mario
 
 object Марио {
-
-    var большойЛи = false
-
-    init {
-        println(
-            "Началось.\nIt’s-a me, Mario!"
-        )
-    }
+    val status = arrayOf("маленький", "большой", "огненный")
+    var stIndex = 0
+    var количествоЖизней = 3
 
     fun съестьГриб() {
-        большойЛи = true
-        println("Марио съел гриб.")
+        if (stIndex == 0) stIndex = 1
+        println("Съел гриб.")
     }
 
-    fun столкнутьсяГумба() {
-        большойЛи = false
-        System.out.println("Марио столкнулся с гумбой.")
+    fun столкнутьсясГумбой() {
+        if (stIndex > 0) stIndex--
+        else количествоЖизней--
+        println("Столкнулся с гумбой.")
     }
 
-    fun isBig() {
-        println("Марио большой - ${Марио.большойЛи}.")
+    fun съестьЗелёныйГриб() {
+        println("Съел зелёный гриб.")
+        количествоЖизней = количествоЖизней + 1
     }
+
+    fun упастьвПропасть() {
+        stIndex = 0
+        количествоЖизней -= 1
+        println("Упал в пропасть.")
+    }
+
+    fun съестьЦветок() {
+        if (stIndex < 2) stIndex++
+        else количествоЖизней++
+        println("Съел цветок.")
+    }
+
+
+    override fun toString() = "Марио ${status[stIndex]}, количество жизней = $количествоЖизней."
+
 }
 
 fun gameOver() {
     println("Game over.")
 }
 
-fun game() {
-    val actions =
-        arrayOf(Марио::isBig, Марио::съестьГриб, Марио::isBig, Марио::столкнутьсяГумба, Марио::isBig, ::gameOver)
-    actions.forEach { it.invoke() }
-}
-
 fun main() {
-    game()
-}
+    val actions = arrayOf(
+        ::gameOver,
+        Марио::съестьГриб,
+        Марио::столкнутьсясГумбой,
+        Марио::съестьЗелёныйГриб,
+        Марио::упастьвПропасть,
+        Марио::съестьЦветок
+    )
 
+    repeat(15) {
+        println(Марио)
+        if (Марио.количествоЖизней == 0) {
+            gameOver()
+            return
+        }
+        var номерДействия = readln().toInt()
+        actions[номерДействия].invoke()
+    }
+    println(Марио)
+    gameOver()
+
+}
